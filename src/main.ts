d0bd902a63e1ebe3e54549e9b9700d3a13257b4f
NACHO <nachodev369@gmail.com>
2025-03-04 22:48:37 +0100

    chore: created dev branch

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3300);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const logger = new Logger('Bootstrap');
+
+  const port = process.env.PORT ?? 3301;
+  await app.listen(port);
+  logger.log(`🚀  Application is running on: http://localhost:${port}/api/v1`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap', err);
+  process.exit(1);
+});
