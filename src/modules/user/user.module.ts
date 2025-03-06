1ef6eabf470323f8001481fa254514775ce18426
David Umoru <umorudavido@gmail.com>
2025-03-06 07:30:10 +0100

    feat: import and register the JwtModule in user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY || 'yourDefaultSecret',
+      signOptions: { expiresIn: process.env.JWT_EXPIRY || '1d' },
+    }),
+  ],
 
   controllers: [UserController],
   providers: [UserService],
