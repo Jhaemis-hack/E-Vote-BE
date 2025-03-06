f4f950fe11bc8082b1fc4150030846d5d5a688bd
adetayo adewobi <adewobiadetayo1@gmail.com>
2025-03-06 04:06:51 +0100

    feat: add login

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,21 +3,61 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UserType } from './entities/user.entity';
+import { LoginDto } from './dto/login-user.dto';
+import { sign, Secret } from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
+import { NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private configService: ConfigService,
+  ) {}
   async registerAdmin(createAdminDto: CreateUserDto) {
     if (createAdminDto.user_type !== UserType.Admin) {
       throw new BadRequestException('Only admins can be registered here.');
     }
 
     const hashedPassword = await bcrypt.hash(createAdminDto.password, 10);
-
-    const newAdmin = {
+    const newAdmin = this.userRepository.create({
       ...createAdminDto,
       password: hashedPassword,
-    };
+    });
+
+    await this.userRepository.save(newAdmin);
 
     return { message: 'Admin registered successfully', data: newAdmin };
+  }
+  async login(payload: LoginDto) {
+    const userExist = await this.userRepository.findOne({
+      where: { email: payload.email },
+      select: ['id', 'email', 'password'],
+    });
+
+    if (!userExist) {
+      throw new BadRequestException('Bad credentials.');
+    }
+
+    const isPasswordValid = await bcrypt.compare(payload.password, userExist.password);
+    if (!isPasswordValid) {
+      throw new BadRequestException('Bad credentials');
+    }
+
+    const { password, ...result } = userExist;
+    return result;
+  }
+
+  async accessToken(user: any): Promise<string> {
+    const secretKey = (this.configService.get<string>('JWT_SECRET_KEY') as Secret) || null;
+
+    if (!secretKey) throw new NotFoundException('JWT_SECRET_KEY is not defined');
+    return sign({ id: user.id, email: user.email }, secretKey, {
+      expiresIn: '1d',
+    });
   }
 
   findAll() {
