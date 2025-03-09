6e8fc1989023abe068faaf20c9c82327bbc5a2fe
zee-ham-su <hsufiian@yahoo.com>
2025-03-09 16:20:29 +0000

    refactor: move IsAfterDate validator to a new location and update logic to allow same day

--- src/modules/common/validators/tests/afterDate.Validator.spec.ts
+++ src/modules/common/validators/tests/afterDate.Validator.spec.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import { validate } from 'class-validator';
-import { IsAfterDate } from '../is-after-date.validator';
+import { IsAfterDate } from '../../../common/validators/is-after-date.validator';
 import { Type } from 'class-transformer';
+
 class TestClass {
   @Type(() => Date)
   startDate: Date;
-  @IsAfterDate('startDate', { message: 'End date must be after start date' })
+
+  @IsAfterDate('startDate', { message: 'End date must be after or on the same day as start date' })
   @Type(() => Date)
   endDate: Date;
 }
+
 describe('IsAfterDate', () => {
   it('should pass validation if endDate is after startDate', async () => {
     const testInstance = new TestClass();
@@ -17,20 +20,21 @@ describe('IsAfterDate', () => {
     const errors = await validate(testInstance);
     expect(errors.length).toBe(0);
   });
+
   it('should fail validation if endDate is before startDate', async () => {
     const testInstance = new TestClass();
     testInstance.startDate = new Date('2025-01-02');
     testInstance.endDate = new Date('2025-01-01');
     const errors = await validate(testInstance);
     expect(errors.length).toBe(1);
-    expect(errors[0].constraints?.isAfterDate).toBe('End date must be after start date');
+    expect(errors[0].constraints?.isAfterDate).toBe('End date must be after or on the same day as start date');
   });
-  it('should fail validation if endDate is equal to startDate', async () => {
+
+  it('should pass validation if endDate is equal to startDate', async () => {
     const testInstance = new TestClass();
     testInstance.startDate = new Date('2025-01-01');
     testInstance.endDate = new Date('2025-01-01');
     const errors = await validate(testInstance);
-    expect(errors.length).toBe(1);
-    expect(errors[0].constraints?.isAfterDate).toBe('End date must be after start date');
+    expect(errors.length).toBe(0);
   });
 });
