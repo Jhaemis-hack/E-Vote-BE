d3afca54767dbca28b7b19966dd06b965cb3889a
Jhaemis-hack <gbemilekekenny@gmail.com>
2025-03-10 17:49:31 +0100

    feat: update response branch

--- src/seed.ts
+++ src/seed.ts
@@ -61,7 +61,7 @@ async function seed() {
   election.end_date = new Date('2023-01-02');
   election.start_time = '09:00:00';
   election.end_time = '17:00:00';
-  election.vote_link = 'http://example.com/vote';
+  election.vote_id = 'http://example.com/vote';
   election.created_by_user = user;
   await electionRepository.save(election);
 
@@ -73,7 +73,7 @@ async function seed() {
   election2.end_date = new Date('2023-02-02');
   election2.start_time = '09:00:00';
   election2.end_time = '17:00:00';
-  election2.vote_link = 'http://example.com/vote2';
+  election2.vote_id = 'http://example.com/vote2';
   election2.created_by_user = user2;
   await electionRepository.save(election2);
 
