1ca87ea73784a916eb0419240a50f47d2935de5c
Ibrahim Fawaz Olamide <connectola@yahoo.com>
2025-03-14 16:29:32 +0100

    feat: implement voters upload via CSV and Excel files

--- src/shared/constants/systemMessages.ts
+++ src/shared/constants/systemMessages.ts
@@ -54,3 +54,4 @@ export const INVALID_VERIFICATION_TOKEN = 'Invalid verification token';
 export const VERIFICATION_TOKEN_EXPIRED = 'Verification token has expired';
 export const WELCOME_EMAIL_FAILED = 'Welcome email failed to send';
 export const ERROR_MAX_CHOICES = 'Maximum choices for single election must be 1';
+export const UPLOAD_VOTER_SUCCESS = 'Voters uploaded successfully';
