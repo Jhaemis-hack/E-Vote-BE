f7feac39c811518aae30d55c9cdf1ecd23ef8607
NACHO <nachodev369@gmail.com>
2025-03-18 14:20:20 +0100

    test: enhance ElectionService tests for sending voting links and improve email service mocks

--- src/modules/candidate/candidate.service.ts
+++ src/modules/candidate/candidate.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-// import { UpdateCandidateDto } from './dto/update-candidate.dto';
+import { UpdateCandidateDto } from './dto/update-candidate.dto';
 
 @Injectable()
 export class CandidateService {
@@ -11,9 +11,10 @@ export class CandidateService {
     return `This action returns a #${id} candidate`;
   }
 
-  // update(id: number, updateUserDto: UpdateCandidateDto) {
-  //   return `This action updates a #${id} candidate`;
-  // }
+  update(id: number, updateUserDto: UpdateCandidateDto) {
+    // Use updateUserDto to update the candidate
+    return `This action updates a #${id} candidate with data: ${JSON.stringify(updateUserDto)}`;
+  }
 
   remove(id: number) {
     return `This action removes a #${id} candidate`;
